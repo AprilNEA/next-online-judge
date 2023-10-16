d470ff9086e757dba8c7611c52904642ae20b52b
AprilNEA <github@sku.moe>
2023-10-16 23:37:46 +0800

    fix: fix daisy to react-daisy

--- packages/frontend/src/components/layout/header.tsx
+++ packages/frontend/src/components/layout/header.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
-import Auth from "@/components/auth"
+import Auth from "@/components/auth";
+import { Button, Dropdown, Menu, Navbar } from "react-daisyui";
 
 const navs = [
   {
@@ -19,10 +22,15 @@ const navs = [
 export default function Header() {
   return (
     <div className="flex justify-center">
-      <div className="navbar bg-base-100 max-w-[1200px]">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+      <Navbar>
+        <Navbar.Start>
+          <Dropdown>
+            <Button
+              tag="label"
+              color="ghost"
+              tabIndex={0}
+              className="lg:hidden"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -33,37 +41,34 @@ export default function Header() {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h18"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h8m-8 6h16"
                 />
               </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
+            </Button>
+            <Dropdown.Menu tabIndex={0} className="w-52 menu-sm mt-3 z-[1]">
               {navs.map((nav) => (
                 <li key={nav.name}>
                   <Link href={nav.path}>{nav.name}</Link>
                 </li>
               ))}
-            </ul>
-          </div>
+            </Dropdown.Menu>
+          </Dropdown>
           <a className="btn btn-ghost normal-case text-xl">NOJ</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+        </Navbar.Start>
+        <Navbar.Center className="hidden lg:flex">
+          <Menu horizontal className="px-1">
             {navs.map((nav) => (
-              <li key={nav.name}>
+              <Menu.Item key={nav.name}>
                 <Link href={nav.path}>{nav.name}</Link>
-              </li>
+              </Menu.Item>
             ))}
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <Auth suppressHydrationWarning/>
-        </div>
-      </div>
+          </Menu>
+        </Navbar.Center>
+        <Navbar.End>
+          <Auth />
+        </Navbar.End>
+      </Navbar>
     </div>
   );
 }
