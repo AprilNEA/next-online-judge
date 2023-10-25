0cf1a0892759b887b988ca497874443ff501c528
AprilNEA <github@sku.moe>
2023-10-25 17:02:08 +0800

    fix: useEffect wrap guard

--- packages/frontend/src/app/guard.tsx
+++ packages/frontend/src/app/guard.tsx
@@ -3,6 +3,7 @@
 import useInfo from "@/hooks/use-info";
 import { usePathname, useRouter } from "next/navigation";
 import Loading from "@/components/layout/loading";
+import { useEffect } from "react";
 
 export default function Guard({ children }: { children: React.ReactNode }) {
   const router = useRouter();
@@ -14,9 +15,11 @@ export default function Guard({ children }: { children: React.ReactNode }) {
     return <Loading />;
   }
 
-  if (userInfo && userInfo.status == "unauthorized") {
-    router.push(`/auth?path=${pathname}`);
-    return;
-  }
+  useEffect(() => {
+    if (userInfo && userInfo.status == "unauthorized") {
+      return router.push(`/auth?path=${pathname}`);
+    }
+  }, [userInfo, pathname]);
+
   return <>{children}</>;
 }
